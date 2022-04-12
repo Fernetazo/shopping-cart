461851155f0901f181458fd7d61bbf1e0b537391
Fernetazo <angelofdarkness88@gmail.com>
2022-04-11 21:40:32 -0300

    Basic bottom bar

--- src/Components/BottomBar.js
+++ src/Components/BottomBar.js
@@ -1,7 +1,11 @@
 function BottomBar() {
   return (
-    <div>
-      <div className="bottomBar">This is BottomBar</div>
+    <div className="bottomBar">
+      <div className="sign">Web design by Fernetazo</div>
+      <div className="option">About</div>
+      <div className="option">FAQ</div>
+      <div className="option">Contact</div>
+      <div className="material-icons">arrow_upward</div>
     </div>
   );
 }
